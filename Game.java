f9f61b5f21153b1c07db5de64e553d045dd77c3d
ngawang30 <ngawang30@gmail.com>
2020-11-27 01:59:55 -0500

    Game Loop

--- Game.java
+++ Game.java
@@ -2,76 +2,86 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.PrintStream;
 import java.util.HashMap;
+import java.util.Random;
 
 public class Game {
 
 
-    public static void main(String[] args)  throws Exception { 
+	public static void main(String[] args)  throws Exception { 
 
-        // READ FROM THE FILE
-        // TODO: 
-        // open file 
-        // create data stucture - hashmap
-        // create two arrays - for countries and for capitals
-        // read file and populate the two arrays
-        // loop through the key array and add to hash map keys
-        // loop through the value array and add to hash map values
-        HashMap<String, String> data = new HashMap<>();
-        String[] countries = new String[196];
-        String[] capitals = new String[196];
-        
-        Scanner file = new Scanner(new File("dataset.txt"));
-        int i = 0;
-        while (file.hasNext()) {
-        
-            String keys, values, line;
+		// READ FROM THE FILE
+		// TODO: 
+		// open file 
+		// create data stucture - hashmap
+		// create two arrays - for countries and for capitals
+		// read file and populate the two arrays
+		// loop through the key array and add to hash map keys
+		// loop through the value array and add to hash map values
+		HashMap<String, String> data = new HashMap<>();
+		String[] countries = new String[196];
+		String[] capitals = new String[196];
 
-            line = file.nextLine(); // get the whole line - string
-            countries[i] = line.substring(0, line.indexOf('-')).trim();
-            capitals[i] = line.substring(line.indexOf('-') + 1, line.length()).trim();
-            i++;
+		Scanner file = new Scanner(new File("dataset.txt"));
+		int i = 0;
+		while (file.hasNext()) {
 
-        }
+			String keys, values, line;
 
-        // populate the map
-        
-        for (int j = 0; j < 195; ++j) {
-            data.put(countries[j], capitals[j]);
-        }
-	
-	Scanner sc = new Scanner(System.in);
-	String name;
-	//Start Game
- 	System.out.print("Welcome To The Geography Game!\nPlease enter the name");
-    System.out.println("of the contestants. When you are done, enter \"done\".");
-    
-    //while (sc.next().compareTo("done") != 0) {
+			line = file.nextLine(); // get the whole line - string
+			countries[i] = line.substring(0, line.indexOf('-')).trim();
+			capitals[i] = line.substring(line.indexOf('-') + 1, line.length()).trim();
+			i++;
+
+		}
+
+		// populate the map
+
+		for (int j = 0; j < 195; ++j) {
+			data.put(countries[j], capitals[j]);
+		}
+
+		Scanner sc = new Scanner(System.in);
+		String name;
+		//Start Game
+		System.out.print("Welcome To The Geography Game!\nPlease enter the name");
+		System.out.println("of the contestants. When you are done, enter \"done\".");
+
+		//while (sc.next().compareTo("done") != 0) {
 
 		//Player nameInput = new Player(sc.next());	
-    //}
-    Player[] players = new Player[2];
-    int k = 0;
-    do{
-        name = sc.next();
-        if (!name.equals("done"))
-            players[k] = new Player(name);
-        k++;
-    } while(name.compareTo("done") != 0); 
-    
-    // game loop
-    //while (players[0].accessScore() < 11 || players[1].accessScore() < 11) {
+		//}
+		Player[] players = new Player[3];
+		int k = 0;
+		do{
+			name = sc.next();
+			if (!name.equals("done"))
+				players[k] = new Player(name);
+			k++;
+		} while(name.compareTo("done") != 0); 
 
-        //for (Player p: players) 
-            //p.showScores();
-    
-    // generate random countries
-    //
+		Random rn = new Random();
+
+		// game loop
+		while (players[0].accessScore() < 11 || players[1].accessScore() < 11) {
+
+			for(int j=0;j<3;j++){
+				String currentCountry = countries[rn.nextInt(196)]; 
+				System.out.print(players[j].getName() + ", it's your turn.  What is the capital of ");
+				System.out.println(currentCountry + "?");
+				players[j].move(currentCountry, data);
+
+			}
+	
+			for (Player p: players) 
+				p.showScores();
+
+			// generate random countries
 
 
-    }
 
+		}
+	}
 }
-
 
 
 
--- Player.java
+++ Player.java
@@ -5,46 +5,48 @@ import java.util.HashMap;
 public class Player {
 
 
-    // public:
-    public Player(String player_name) {
-        this.points = 0;
-        this.name = player_name;
-    }
+	// public:
+	public Player(String player_name) {
+		points = 0;
+		name = player_name;
+	}
 
-    public void move(String country, HashMap<String, String> data) {
-        //System.out.println(Game.data);
-        Scanner input = new Scanner(System.in);
-        String answer = data.get(country); // this is the answer
-        String guess = input.nextLine().trim(); //user input
-        if (guess == answer) {
-            points++;
-            System.out.println("You are correct you loser :) \n");
-        } else {
-            System.out.println("You are wrong, and you are still a loser :( \n");
-            if (points != 0) {
-                points--;
-            } else {
-                System.out.println("How do you have zero points. SMH (-_-) \n");
-            }
-        }
-    }
+	public void move(String country, HashMap<String, String> data) {
+		//System.out.println(Game.data);
+		Scanner input = new Scanner(System.in);
+		String answer = data.get(country); // this is the answer
+		String guess = input.nextLine().trim(); //user input
+		if (guess.equalsIgnoreCase(answer)) {
+			points++;
+			System.out.println("You are correct you loser :) \n");
+		} else {
+			System.out.println("You are wrong, and you are still a loser :( \n");
+			if (points != 0) {
+				points--;
+			} else {
+				System.out.println("How do you have zero points. SMH (-_-) \n");
+			}
+		}
+	}
 
-    public void showScores() {
-        System.out.println(String.format("%10s's score: %10d \n",name, points));
-    }
+	public void showScores() {
+		System.out.println(String.format("%10s's score: %10d \n",name, points));
+	}
 
-    public void accessScore() {
-        return this.points;
-    }
+	public int accessScore() {
+		return (this.points);
+	}
+
+	public String getName() {
+		return (name);
+	}
 
 
 
+	// private: 
 
-
-    // private: 
-
-    private int points;
-    private String name;
+	private int points;
+	private String name;
 
 
 
